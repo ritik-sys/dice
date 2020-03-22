b34995665476f581a0791fe9c785aaf43c1f9310
ritik-sys <60854557+ritik-sys@users.noreply.github.com>
2020-03-22 23:09:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,16 +10,16 @@ randomNumber2=Math.floor((6*randomNumber2)+1);
 var imagenumber="dice"+randomNumber2+".png";
 var randomsource2="images/" + imagenumber;
 image2.setAttribute("src",randomsource2);
-if(randomNumber1===randomNumber2)
-{
-  document.querySelector("h1").innerHTML="game draws";
-}
-else if(randomNumber1>randomNumber2)
+if(randomNumber1>randomNumber2)
 {
   document.querySelector("h1").innerHTML="player 1 wins!";
 
 }
-else
+if(randomnumber1<randomnumber2)
 {
   document.querySelector("h1").innerHTML="player 2 wins!";
+}
+if(randomNumber1===randomNumber2)
+{
+  document.querySelector("h1").innerHTML="game draws!!";
 }
